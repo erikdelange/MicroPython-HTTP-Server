7ae4ece106f21054c64009b070736df991310476
Erik de Lange <erikdelange@users.noreply.github.com>
2022-10-29 14:50:07 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Minimal servers for handling HTTP requests. Included are a single-threaded and a
 
 For every combination of method plus path (like "GET" and "/index") which must be handled by the HTTP server a function must be declared, such as *root* in the example below. By preceding the function definition with decorator @route the function is registered as the handler for the specified method-path combination. In this way the code for the server itself remains hidden and generic; you only need to define the handlers.
 
-Intentionally extremely simple to keep the code as small as possible. HTTP requests are presented as a dict to the handlers (see and run *url.py* for the exact content and a demo), but HTTP responses must be crafted line by line (or by using some predefined ones from *response.py*).
+Intentionally extremely simple to keep the code as small as possible. HTTP requests are presented as a dict to the handlers (see and run *url.py* for the exact content and a demo), but HTTP responses must be crafted line by line (or by using predefined ones from *response.py*).
 
 ``` Python
 import uasyncio as asyncio
@@ -25,14 +25,16 @@ async def root(reader, writer, request):
 
 
 if __name__ == "__main__":
-    asyncio.run(app.start())  # does not return
+    loop = asyncio.get_event_loop()
+    loop.create_task(app.start())
+    loop.run_forever()
 ```
 To see the async version of the server working start *ademo.py* via the REPL and fire HTTP requests at it via Postman, Insomnia or similar. The demo code includes two divide-by-zero exceptions so you can see how they are handled.
 
 Yes, there are many better and functionally richer examples available on GitHub, but for learning the structure of HTTP requests and also a bit about uasyncio this code served me well. For a detailled understanding of uasyncio see the excellent GitHub pages of [Peter Hinch](https://github.com/peterhinch/micropython-async/blob/master/v3/docs/TUTORIAL.md).
 
-## Differences between httpserver and ahttpserver
-### httpserver
+### Differences between httpserver and ahttpserver
+httpserver
 - Is single threaded. Is suggest to use ahttpserver whenever possible as it supports cooperative multitasking.
 - Also works on Pycom's WiPy's. At the time of writing (2021) Pycom's MicroPython version is lagging behind and does not has asyncio and f-strings included which are required by ahttpserver.
 - Can be stopped gracefully by adding the line below in your code. Can come in handy during development.
--- ademo.py
+++ ademo.py
@@ -1,8 +1,8 @@
-import sys
 import time
+
 import uasyncio as asyncio
 
-from ahttpserver import sendfile, Server
+from ahttpserver import Server, sendfile
 
 app = Server()
 
@@ -18,7 +18,7 @@ async def root(reader, writer, request):
     try:
         print(1/0)
     except Exception as e:
-        print("exception in function root():", e)  # exception handled locally
+        print("exception in function root():", e)  # exception handled locally, does not stop server
 
 
 # @app.route("GET", "/")  # if uncommented raises route already declared exception
@@ -45,11 +45,12 @@ async def get_time(reader, writer, request):
     await writer.drain()
     t = time.localtime()
     writer.write(f"{t[2]:02d}-{t[1]:02d}-{t[0]:04d} {t[3]:02d}:{t[4]:02d}:{t[5]:02d}")
-    print(1/0)  # will be caught by global exception handler
+    print(1/0)  # will be caught by global exception handler, stops server (and the rest)
 
 
 @app.route("GET", "/api/stop")
 async def stop(reader, writer, request):
+    """ Force asyncio scheduler to stop, just like ctrl-c on the repl """
     writer.write(b"HTTP/1.1 200 OK\r\n")
     writer.write(b"Connection: close\r\n")
     writer.write(b"\r\n")
@@ -58,7 +59,7 @@ async def stop(reader, writer, request):
 
 
 async def hello():
-    """ For demo purposes show system is still alive """
+    """ Show system is still alive """
     count = 0
     while True:
         print("hello", count)
@@ -66,22 +67,22 @@ async def hello():
         await asyncio.sleep(60)
 
 
-def set_global_exception_handler():
-    def handle_exception(loop, context):
-        # uncaught exceptions raised in route handlers end up here
-        print("global exception handler:", context)
-        sys.print_exception(context["exception"])
-
-    loop = asyncio.get_event_loop()
-    loop.set_exception_handler(handle_exception)
-
-
 if __name__ == "__main__":
     try:
-        set_global_exception_handler()
+        def handle_exception(loop, context):
+            # uncaught exceptions end up here
+            import sys
+            print("global exception handler:", context)
+            sys.print_exception(context["exception"])
+            sys.exit()
 
-        asyncio.create_task(hello())
-        asyncio.run(app.start())  # must be last, does not return
+        loop = asyncio.get_event_loop()
+        loop.set_exception_handler(handle_exception)
+
+        loop.create_task(hello())
+        loop.create_task(app.start())
+
+        loop.run_forever()
     except KeyboardInterrupt:
         pass
     finally:
